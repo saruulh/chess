750a399ac3f458c208fabcc2420c5dd5035e0e82
Alex Satterly <alcsatt@gmail.com>
2015-05-29 15:23:58 -0400

    Added all the piece classes

---
+++ pieces/knight.rb
@@ -0,0 +1,30 @@
+require_relative '../piece'
+require_relative '../board'
+
+class Knight < Piece
+
+  def initialize(args)
+    @curr_pos = args[:curr_pos]
+    @color = args[:color].to_sym
+    @board = args[:board]
+    @valid_moves = []
+    @move_vector = [-17, -15, -10, -6, 6, 10, 15, 17]
+  end
+
+
+  def get_valid_moves(delta)
+    new_pos = @curr_pos + delta
+    if (0..63).include?(new_pos)
+      return new_pos if @board[new_pos].opponent?
+      return if @board[new_pos].ally?
+      return new_pos
+    end
+  end
+
+  def valid_move_loop
+    @move_vector.each do |delta|
+      @valid_moves = get_valid_moves(delta)
+    end
+  end
+
+end
---
+++ pieces/pawn.rb
@@ -0,0 +1,49 @@
+require_relative '../piece'
+require_relative '../board'
+
+class Pawn < Piece
+
+  attr_reader :curr_pos, :color
+
+  def initialize(args)
+    @curr_pos = args[:curr_pos]
+    @color = args[:color].to_sym
+    @board = args[:board]
+    @valid_moves = []
+  end
+
+  def move_vector
+    if @color == :w
+      @move_vector = [7, 8, 9]
+      @move_vector << 16 if (8..15).include?(@curr_pos)
+    else
+      @move_vector = [-9, -8, -7]
+      @move_vector << -16 if (48..55).include?(@curr_pos)
+    end
+  end
+
+  def get_valid_moves(delta)
+    new_pos = @curr_pos + delta
+    moving_forward = delta == 8 || delta == -8
+    if (0..63).include?(new_pos)
+      if moving_forward && (@board[new_pos].opponent? || @board[new_pos].ally?)
+         return
+      elsif @board[new_pos].opponent?
+         return new_pos
+      elsif @board[new_pos].ally?
+        return
+      elsif moving_forward
+        return new_pos
+      end
+    end
+  end
+
+  def valid_move_loop
+    move_vector
+    @move_vector.each do |delta|
+      @valid_moves << get_valid_moves(delta)
+    end
+  end
+
+end
+
